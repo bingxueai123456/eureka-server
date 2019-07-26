5983460555b40c7c2b3e6a97a8bc8c0771a0280f
dongbc <dongbc@silvrr.com>
2019-07-26 09:04:15 +0800

    Initial commit

---
+++ src/main/java/com/ice/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: eclair
+ * @Description:
+ * @Date: 2019/7/25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
